f8064aba829e096dd1beca893e83e00716b5096a
mrber <mr.bergeron83@gmail.com>
2026-01-29 23:54:01 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -82,6 +82,7 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     git
+    wl-clipboard
     wget
     os-prober
     nodejs_24
